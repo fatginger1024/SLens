5cd61fc9d0e0542d794e86928b7fb04258ff4db8
Qing <zhouqing@qudoor.cn>
2022-04-20 17:06:19 +0800

    update unittests

--- tests/test_models.py
+++ tests/test_models.py
@@ -8,13 +8,13 @@ class tests(unittest.TestCase):
         
     def test_mass1(self):
         galaxy = Sersic()
-        self.assertAlmostEqual(galaxy.M2d(galaxy._Re)/(galaxy._Mstar/2),1,places=2)
+        self.assertAlmostEqual(galaxy.M2d(galaxy._Re)/(galaxy._Mstar/2),1,places=5)
 
     def test_mass2(self):
         galaxy = Sersic()
         inte_func = lambda R: galaxy.Sigma(R)*R
         inte_out = quad(inte_func,0,galaxy._Re)[0]
-        self.assertAlmostEqual(2*pi*inte_out/(galaxy._Mstar/2),1,places=2)
+        self.assertAlmostEqual(2*pi*inte_out/(galaxy._Mstar/2),1,places=5)
  
     
 if __name__ == '__main__':
