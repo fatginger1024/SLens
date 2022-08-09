572025db461da6ea9ad715b633afcb43bfe16f1f
Qing <zhouqing@qudoor.cn>
2022-08-09 16:43:05 +0800

    init new functions

--- SLens/__init__.py
+++ SLens/__init__.py
@@ -4,9 +4,10 @@ import SLens.load_data
 import SLens.cp_model
 import SLens.analyse
 import SLens.simulation
+import SLens.preprocess
 
 from SLens.models import gnfw, Distances, Sersic, mass_size, concentration
 from SLens.load_data import tables, fxgx, ReConc_loader, load_MICE,load_COSMOS
 from SLens.cp_model import gnfwSersic
 from SLens.analyse import analyser
-from SLens.simulation import SimRun, run_mocks
+from SLens.simulation import SimRun
